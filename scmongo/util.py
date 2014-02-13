4c3cad52c237966c061322ddc2dc1961cdbf8787
Matthew Foy <mfoy@allclasses.com>
2014-02-13 14:28:37 -0500

    Updated connection settings.

--- scmongo/util.py
+++ scmongo/util.py
@@ -7,17 +7,8 @@ def get_database(settings):
     defined in the settings.
     """
 
-    host = settings['HTTPCACHE_MONGO_HOST'] \
-        or settings['MONGO_HOST'] \
-        or os.environ.get('MONGO_HOST') \
-        or 'localhost'
-    port = settings.getint('HTTPCACHE_MONGO_PORT') \
-        or settings.getint('MONGO_PORT') \
-        or int(os.environ.get('MONGO_PORT', '27017')) \
-        or 27017
-    db = settings['HTTPCACHE_MONGO_DATABASE'] \
-        or settings['MONGO_DATABASE'] \
-        or os.environ.get('MONGO_DATABASE') \
-        or settings['BOT_NAME']
+    uri = settings['HTTPCACHE_MONGODB_URI']
+    db = settings['HTTPCACHE_MONGODB_DATABASE']
 
-    return pymongo.Connection(host, port)[db]
+    return pymongo.MongoClient(uri)[db]
+
